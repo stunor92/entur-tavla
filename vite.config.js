6dc5679810364128fcd8398ac4797df7dfd1b945
Sturle Tolo Nordeide <110419122+stunor92@users.noreply.github.com>
2025-12-23 15:07:30 +0100

    vite config to js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+// ...existing code...
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	base: '/entur-tavla/',
-	publicDir: 'static'
+  plugins: [react(), tailwindcss()],
+  base: '/entur-tavla/',
+  publicDir: 'static'
 });
+// ...existing code...
+
